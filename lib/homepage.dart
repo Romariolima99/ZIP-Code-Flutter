f4ae2a61af5aee91b0d92e9ab4a56b00c50f83bb
Romário Lima <devromariolima@gmail.com>
2024-05-30 20:20:44 -0300

    Style: Finalização do Layout

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import "package:http/http.dart" as http;
+import "dart:convert";
+
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,21 +17,76 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Consulta de Cep'),
-        backgroundColor: Colors.black87,
+        title: const Text(
+          'Consultar Cep',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       body: Container(
-        padding: EdgeInsets.all(50),
-        child: const Column(
+        padding: const EdgeInsets.all(50),
+        child: Column(
           children: [
-            TextField(
+            SizedBox(
+              height: 350,
+              width: 350,
+              child:
+                  Image.asset("assets/Screenshot_1.png"), // Caminho corrigido
+            ),
+            const SizedBox(height: 20),
+            const TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(border: OutlineInputBorder(),
-              labelText: "Digite seu cep",
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Digite seu cep",
               ),
               style: TextStyle(fontSize: 16),
-            )
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Ação quando o botão for pressionado
+              },
+              child: const Text(
+                'Consultar',
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'Resultado',
+              style: TextStyle(fontSize: 18),
+            ),
           ],
         ),
       ),
